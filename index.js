a1d223c8fa7610b0c62d28d7f065e1f069494494
batoul sudki tenbakjy <batoul.tenbakji@gmail.com>
2022-10-19 19:27:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    const arr = [];
+    for (let i = 0; i < name.length; i++) {
+        arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return arr;
+}
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        --num;
+    }
+}
